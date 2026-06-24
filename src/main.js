e7b167c9ca1dd9f48d36a9e18a9acc1149cc9e30
Eric Hanaway <erichanaway@yahoo.com>
2026-06-23 18:55:03 -0700

    Create Excel reader

--- src/main.js
+++ src/main.js
@@ -1,2 +1,43 @@
 import './style.css';
-console.log('HMIS Data Quality App loaded');
+import * as XLSX from 'xlsx';
+
+const fileInput = document.querySelector('input[type="file"]');
+
+fileInput.addEventListener('change', handleFile);
+
+function handleFile(event) {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload =(e) => {
+        const data = new Uint8Array(e.target.result);
+
+        const workbook = XLSX.read(data, {
+            type: 'array'
+        });
+
+        const sheet =
+            workbook.Sheets[
+                workbook.SheetNames[0]
+            ];
+        
+            const rows =
+                XLSX.utils.sheet_to_json(sheet);
+
+            console.table(rows);
+            
+            console.table(rows[0]);
+
+            console.log(rows[0].Severity);
+
+            console.log(
+                `Loaded ${rows.length} rows`
+            );
+    };
+
+    reader.readAsArrayBuffer(file);
+}
+
